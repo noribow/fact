02f91cb57ce102e85ace14369b3480c9f532e281
Takanori SHIRAI <takanori.shirai+github@gmail.com>
2015-03-14 23:58:29 +0900

    Modify:indent style:webkit

--- main2.c
+++ main2.c
@@ -4,25 +4,32 @@
 #include <gmp.h>
 
 typedef struct {
-  int flag;
-  int fact_count;
-  mpz_t n;
-  mpz_t *fact;
-  int *fact_flag;
-}FACTOR_ARRAY;
+    int flag;
+    int fact_count;
+    mpz_t n;
+    mpz_t* fact;
+    int* fact_flag;
+} FACTOR_ARRAY;
 
 #define FACTOR_ARRAY_ELEMENT_MAX (100)
 
 #define NUMBER_PRIME (0)
 #define NUMBER_NOT_PRIME (1)
+#define NUMBER_COMPOSITE (2)
+#define NUMBER_NOT_COMPOSITE (3)
+
+#define 
+int *prime_table;
+int prime_table_size;
+
 
 int prime_test(mpz_t n)
 {
-  return 0;
+    return 0;
+    test();
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-  return 0;
+    return 0;
 }
-
