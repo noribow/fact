c5abd3f0f04f86a32245d3fa126e6f9a1f900158
Takanori SHIRAI <takanori.shirai+github@gmail.com>
2015-03-16 05:37:05 +0900

    Add:prime sieve

--- main2.c
+++ main2.c
@@ -18,18 +18,65 @@ typedef struct {
 #define NUMBER_COMPOSITE (2)
 #define NUMBER_NOT_COMPOSITE (3)
 
-#define 
-int *prime_table;
+#define MAX_PRIME_SIEVE_SIZE (10 * 1000 * 1000)
+int* prime_table;
 int prime_table_size;
+char* prime_sieve_table;
 
+int make_prime_table(void)
+{
+    int prime_count = 1;
+    int i, j, p;
+
+    prime_sieve_table = (char*)malloc(MAX_PRIME_SIEVE_SIZE);
+    if (prime_sieve_table == NULL)
+        return 0;
+
+    for (i = 0; i < MAX_PRIME_SIEVE_SIZE; i++)
+        prime_sieve_table[i] = 1;
+
+    for (i = 0; i < MAX_PRIME_SIEVE_SIZE; i++) {
+        if (prime_sieve_table[i] == 0)
+            continue;
+
+        prime_count++;
+        p = 2 * i + 3;
+        for (j = i + p; j < MAX_PRIME_SIEVE_SIZE; j += p)
+            prime_sieve_table[j] = 0;
+    }
+
+    prime_table = (int*)malloc(sizeof(int) * prime_count);
+    if (prime_table == NULL)
+        return 0;
+
+    prime_table[0] = 2;
+    prime_count = 0;
+    for (i = 0; i < MAX_PRIME_SIEVE_SIZE; i++) {
+        if (prime_sieve_table[i] == 0)
+            continue;
+
+        p = 2 * i + 3;
+        prime_count++;
+        prime_table[prime_count] = p;
+    }
+    prime_table_size = prime_count + 1;
+    free(prime_sieve_table);
+
+    return 0;
+}
 
 int prime_test(mpz_t n)
 {
     return 0;
-    test();
 }
 
 int main(int argc, char* argv[])
 {
+    int i;
+
+    make_prime_table();
+    for (i = 0; i < prime_table_size; i++)
+        printf("prime[%d] : %d\r\n", i, prime_table[i]);
+
     return 0;
 }
